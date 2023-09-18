d8e248b6ae14926ed876d0ad72e8cc466417e833
devgoncalves98 <gggabrielgoncalves.98@gmail.com>
2023-09-18 17:43:53 -0300

    Criando pagina inicial da tela de inserção de URL

---
+++ app/Views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <title>Pagina Inicial</title>
+</head>
+
+<body>
+  <section class="align-middle m-3">
+    <div class="bg-light border container text-center">
+      <div>
+        <h2 class="m-4">Selecione a pagina que deseja buscar</h2>
+        <form method="get" class="m-4">
+          <label for="url">Url do Site desejado: </label>
+          <input type="url" name="url" id="url">
+          <button type="submit">Seguir</button>
+        </form>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
